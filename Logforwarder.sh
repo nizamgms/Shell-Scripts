b3cd31332867e8bc5f141e6734dc57385480e214
Nizam <mdnizam.gms@gmail.com>
2023-10-25 20:19:08 +0530

    Update Logforwarder.sh

--- Logforwarder.sh
+++ Logforwarder.sh
@@ -25,6 +25,13 @@ s3_secret_key=<your s3 secret key>
 #prepare signature hash to be sent in Authorization header
 signature_hash=`echo -en ${signature_string} | openssl sha1 -hmac ${s3_secret_key} -binary | base64`
 
+# curl command to upload file to s3
+curl -X PUT -T "${file_to_upload}" \
+  -H "Host: ${bucket}.s3.amazonaws.com" \
+  -H "Date: ${dateValue}" \
+  -H "Content-Type: ${contentType}" \
+  -H "Authorization: AWS ${s3_access_key}:${signature_hash}" \
+  https://${bucket}.s3.amazonaws.com/${file_to_upload}
 
 =======================================================================================================
 
